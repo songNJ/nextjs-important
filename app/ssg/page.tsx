d3dc11b5bef41c1760bde995eba685aa42463c75
姜年松 <39901091+songNJ@users.noreply.github.com>
2025-11-27 16:38:09 +0800

    Update page.tsx

--- app/ssg/page.tsx
+++ app/ssg/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div>
       <Nav></Nav>
-      <h1>SSG 页面1</h1>
+      <h1>SSG 页面21</h1>
       <p>构建时静态生成，不随请求变化。</p>
       <p>生成时间: {now}</p>
     </div>
